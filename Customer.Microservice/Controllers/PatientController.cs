e82fe774d6943f837984a3fde5c01dcc0c8a932e
phani sekhar <veesam.phanisekhar01@gmail.com>
2021-06-07 21:52:51 +0530

    refactoring the files

--- Customer.Microservice/Controllers/PatientController.cs
+++ Customer.Microservice/Controllers/PatientController.cs
@@ -1,13 +1,8 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Newtonsoft.Json;
 
 namespace Customer.Microservice.Controllers
 {
@@ -26,32 +21,32 @@ namespace Customer.Microservice.Controllers
 
             for (int i = 0; i < arrays.Length; i++)
             {
-                for (int j = 0; j < arrays[i].Length-1; j++)
+                for (int j = 0; j < arrays[i].Length - 1; j++)
                 {
-                    if (j < arrays[i].Length-1)
-                        if((arrays[i][j] == arrays[i][j + 1]) && (arrays[i][j] ==1 && arrays[i][j + 1] ==1))
-                        count++;
+                    if (j < arrays[i].Length - 1)
+                        if ((arrays[i][j] == arrays[i][j + 1]) && (arrays[i][j] == 1 && arrays[i][j + 1] == 1))
+                            count++;
 
                 }
-                
+
             }
 
             int noOfColumns = arrays[0].Length;
-            for (int j = 0; j <= noOfColumns-1; j++)
+            for (int j = 0; j <= noOfColumns - 1; j++)
             {
                 for (int i = 0; i < arrays.Length; i++)
                 {
                     if (i < arrays.Length - 1)
-                        if ((arrays[i][j] == arrays[i + 1][j]) && (arrays[i][j] ==1 && arrays[i + 1][j] ==1))
-                        count++;
+                        if ((arrays[i][j] == arrays[i + 1][j]) && (arrays[i][j] == 1 && arrays[i + 1][j] == 1))
+                            count++;
                 }
-                
+
             }
 
-            
-            int obliqueCount= Oblique(arrays, arrays.Length, noOfColumns);
+
+            int obliqueCount = Oblique(arrays, arrays.Length, noOfColumns);
             count = obliqueCount + count;
-                       
+
             return Ok(count);
         }
 
@@ -60,7 +55,7 @@ namespace Customer.Microservice.Controllers
         {
             int count = 0;
             bool indicator = false;
-           
+
             for (int k = 0; k <= rows - 3; k++)
             {
                 for (int l = 0; l <= cols - 3; l++)
@@ -69,7 +64,7 @@ namespace Customer.Microservice.Controllers
                     {
                         if ((matrix[k][l] == matrix[k + 1][l + 1]) && (matrix[k][l] == 1 && matrix[k + 1][l + 1] == 1))
                             indicator = true;
-                        if ((indicator = true) && ((matrix[k][l] == matrix[k + 1][l + 1]) && matrix[k][l] == matrix[k + 2][l + 2] ) && (matrix[k][l] == 1 && matrix[k + 1][l + 1] == 1))
+                        if ((indicator = true) && ((matrix[k][l] == matrix[k + 1][l + 1]) && matrix[k][l] == matrix[k + 2][l + 2]) && (matrix[k][l] == 1 && matrix[k + 1][l + 1] == 1))
                         {
                             indicator = true;
                             count++;
@@ -84,7 +79,7 @@ namespace Customer.Microservice.Controllers
             return count;
         }
 
-                // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
+        // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
         public class Root
         {
             public List<List<int>> matrix { get; set; }
